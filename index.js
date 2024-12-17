886a0fa4b93a2f4bd87f4c69500d6a0ec39eca0c
Josh Cole <jcole7755@gmail.com>
2024-12-16 22:30:32 -0600

    Car Rental App JS

---
+++ index.js
@@ -0,0 +1,12 @@
+function openMenu() {
+    const body = document.body;
+  
+    body.classList.add("open");
+  }
+  
+  function closeMenu() {
+    const body = document.body;
+  
+    body.classList.remove("open");
+  }
+  
